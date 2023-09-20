f17b3615ead07ccf877e952ac405cfdddf6f97aa
Ben Solwitz <ben.solwitz@gmail.com>
2023-09-20 11:01:20 -0400

    first commit implemented basic chat interface with ConversationBufferMemory

---
+++ main.py
@@ -0,0 +1,42 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+
+llm = ChatOpenAI()
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a nice chatbot having a conversation with a human."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+)
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name.
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=False,
+    memory=memory
+)
+
+
+def main():
+    print("please input a message for the llm!")
+    while True:
+        human_input = input()
+        llm_response = conversation({"question": human_input})
+        print(llm_response['text'])
+
+
+main()
